8db4a6c71b5151c2e69dd52d12b53899b2300e54
johanelbg <johan.elberg38@gmail.com>
2019-06-22 20:40:39 +0300

    completed companies CRUD

--- routes/companies.js
+++ routes/companies.js
@@ -11,12 +11,34 @@ router.get("/", (req, res) =>
     .catch(e => console.log(e))
 );
 
+router.get("/:company_id", (req, res) => {
+  Companies.findByPk(req.params.company_id)
+    .then(company => res.send(company))
+    .catch(e => console.log(e));
+});
+
 router.post("/", (req, res) => {
   const { name } = req.body;
   Companies.create({
     name
   })
     .then(() => res.sendStatus(201))
+    .catch(e => console.log(e));
+});
+
+router.delete("/:company_id", (req, res) => {
+  Companies.destroy({
+    where: { id: req.params.company_id }
+  })
+    .then(company_id => res.send(`company with id: ${company_id} removed`))
+    .catch(e => console.log(e));
+});
+
+router.put("/:company_id", (req, res) => {
+  const { name } = req.body;
+
+  Companies.update({ name }, { where: { id: req.params.company_id } })
+    .then(company => res.send(company))
     .catch(e => console.log(e));
 });
 
